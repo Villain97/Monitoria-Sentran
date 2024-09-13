78e0722630a50db07458fdee240494c3b1660fbc
Mateus Andrade <132583430+Villain97@users.noreply.github.com>
2024-09-13 12:49:35 -0300

    Arquivos disponibilizados via github

---
+++ 0-main.js
@@ -0,0 +1,85 @@
+document.write(`
+    <!DOCTYPE html>
+    <html lang="pt-br"> 
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Monitoria Sentran</title>
+            <link rel="stylesheet" href="0-style.css">
+        </head>
+        <body>
+            <div id="container">
+                <div id="topBar">Monitoramento Sentran</div>
+                <div id="headerArea">
+                    <div id="header" style="height: 157px;"></div>
+                    <nav id="navbar" style="margin-bottom: 10px;">
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="2-velsis.html">Velsis</a></li>
+                            <li><a href="3-splice.html">Splice</a></li>
+                            <li><a href="4-perkons.html">Perkons</a></li>
+                            <li><a href="https://sentran-rc-puma01.eastus.cloudapp.azure.com/" target="_blank">Pumatronix</a></li>
+                            <li><a href="5-suporte.html">Suporte</a></li>
+                        </ul>
+                    </nav>
+                </div>
+    
+                <div id="flex">
+                    <aside id="leftSidebar" style="margin-right: 10px;">
+                        <h2 class="atualizações">Atualizações</h2>
+                        <div class="box">
+                            <ul style="padding-left:10px;">
+                                <li class="atualizações__p">12/09 Equipamentos perkons adicionado </li>
+                                <li class="atualizações__p">10/09 Aba de comandos finalizada </li>
+                                <li class="atualizações__p">09/09 código atualizado adicionado javascript</li>
+                                <li class="atualizações__p">04/09 Linha de codigo reescrita</li>
+                                <li class="atualizações__p">03/09 Equipamentos 1460 e 1461 adicionados</li>
+                                <li class="atualizações__p">03/09 Equipamentos de Jaguariuna adicionados</li>  
+                            </ul>
+                        </div>
+                        <h2 class="atualizações">Outros Projetos</h2>
+
+                        <ul>
+                            <li class="atualizações__p"><a href="https://velvetblue.neocities.org" class="velvetblue"><img src="https://files.catbox.moe/vltthb.png" alt="Velvet Blue"></a></li>
+                        </ul>
+                        <!-- Código de fase da lua -->
+                        <table cellpadding="0" cellspacing="0" border="0" width="214">
+                            <tr>
+                                <td align="center"><img src="https://www.moonmodule.com/cs/dm/s.gif" width="1" height="136" border="0" alt="" /></td>
+                                <td background="https://www.moonmodule.com/cs/dm/hs.gif" valign="bottom" align="center">
+                                    <a href="https://www.moonconnection.com" target="mc_moon_ph">
+                                        <img src="https://www.moonmodule.com/cs/dm/s.gif" width="212" height="90" border="0" alt="" />
+                                    </a>
+                                    <table cellpadding="0" cellspacing="0" border="0" width="204" height="20">
+                                        <tr>
+                                            <td align="center" bgcolor="#000000">
+                                                <a href="https://www.moonconnection.com/moon_phases.phtml" target="mc_moon_ph">
+                                                    <font color="#7F7F7F" size="1" face="arial,helvetica,sans-serif">
+                                                        <span style="color:#7F7F7F;font-family:arial,helvetica,sans-serif;font-size:10px;">lunar phase</span>
+                                                    </font>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <img src="https://www.moonmodule.com/cs/dm/s.gif" width="1" height="4" border="0" alt="" />
+                                </td>
+                                <td><img src="https://www.moonmodule.com/cs/dm/s.gif" width="1" height="136" border="0" alt="" /></td>
+                            </tr>
+                        </table>
+                    </aside>
+                    <main>
+                    </main>   
+                </div>
+                <footer id="footer" style="margin-top: 10px;">
+                    <h2 class="rodape__texto">Sentran 2024 - <a href="https://neocities.org/"> <img src="neocities.png" width="100px"></a></h2>
+                    <h2 class="rodape__texto">Desenvolvido e criado por Mateus Andrade</h2>
+                </footer>
+            </div>
+        </body>
+    </html>
+    `);
+    
+
+
+
